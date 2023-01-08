6884b849d75dc870026bafcd6fc5c50fcc8c23d0
Kaelin Laundry <wasabifan@outlook.com>
2023-01-07 19:47:35 -0800

    Introduce barriers before/after SCTLR writes
    Currently conservative; all SCTLR writes will apply the barriers, even
    if not necessary for the change made. Barriers alone aren't sufficient
    for memory coherence -- caches will need to be enabled/disabled/flushed
    by calling code in some cases.

--- libs/cortex-r5-pac/src/registers/macros.rs
+++ libs/cortex-r5-pac/src/registers/macros.rs
@@ -33,8 +33,31 @@ macro_rules! impl_readable_for_coprocessor_register {
     }
 }
 
+// TODO: Theoretically, only need barriers around modifications that affect specific bits in the register.
+// Consider ways to avoid unnecessary barriers for other bits.
+
 macro_rules! impl_writeable_for_coprocessor_register {
+    (volatile $regtype:ty: $bitfield_type:ty, $cpnum:tt, $opc1:tt, $crn:tt, $crm:tt, $opc2:tt) => {
+        impl_writeable_for_coprocessor_register!(
+            $regtype: $bitfield_type, $cpnum, $opc1, $crn, $crm, $opc2,
+            // Serialize all in-flight data transactions before we potentially modify memory mappings
+            "dsb",
+            // Ensure all future instruction fetches use new memory map.
+            // This probably assumes that this instruction is in the same MPU region/page as the reg write.
+            "isb",
+            // Since this config change might affect arbitrary memory mappings, can't provide nomem/nostack
+            options()
+        );
+    };
     ($regtype:ty: $bitfield_type:ty, $cpnum:tt, $opc1:tt, $crn:tt, $crm:tt, $opc2:tt) => {
+        impl_writeable_for_coprocessor_register!(
+            $regtype: $bitfield_type, $cpnum, $opc1, $crn, $crm, $opc2,
+            "",
+            "",
+            options(nomem, nostack)
+        );
+    };
+    ($regtype:ty: $bitfield_type:ty, $cpnum:tt, $opc1:tt, $crn:tt, $crm:tt, $opc2:tt, $pre_barriers:literal, $post_barriers:literal, options($($asm_opts:tt),*)) => {
         impl tock_registers::interfaces::Writeable for $regtype {
             type T = u32;
             type R = $bitfield_type;
@@ -43,6 +66,7 @@ macro_rules! impl_writeable_for_coprocessor_register {
             #[inline]
             fn set(&self, value: Self::T) {
                 unsafe {
+                    core::arch::asm!($pre_barriers);
                     core::arch::asm!(
                         concat!(
                             "mcr ", stringify!($cpnum),
@@ -53,8 +77,9 @@ macro_rules! impl_writeable_for_coprocessor_register {
                             ", #", $opc2,
                         ),
                         reg = in(reg) value,
-                        options(nomem, nostack),
+                        options($($asm_opts),*),
                     );
+                    core::arch::asm!($post_barriers);
                 };
             }
 
